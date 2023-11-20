1077ea7409e0b4684941536ff737be06a1194144
Oksana Sheremet <sheremet5631@gmail.com>
2023-11-20 14:36:37 +0300

    added urls

--- test_cadaster_query/requests/views.py
+++ test_cadaster_query/requests/views.py
@@ -1,11 +1,32 @@
+import time
+
 from django.http import JsonResponse
-from django.shortcuts import render
-from django.views.decorators import csrf_exempt
 
 from .models import CadasterRequest
 
 
-@csrf_exempt
+def query(request):
+    if request.method == 'POST':
+        data = request.POST
+        cadaster_number = data.get('cadaster_number')
+        latitude = data.get('latitude')
+        longitude = data.get('longitude')
+
+        time.sleep
+
+        result = True
+
+        request_data = CadasterRequest(
+            cadaster_number=cadaster_number,
+            latitude=latitude,
+            longitude=longitude,
+            result=result
+        )
+        request_data.save()
+
+        return JsonResponse({'result': result})
+
+
 def result(request):
     if request.method == 'POST':
         data = request.POST
